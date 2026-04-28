5557727ac093dcef7adf9e7dc33248ac24c4a65e
shivam24s <shivamshrivastav8055@gmail.com>
2026-04-28 16:06:01 +0530

    server code added

---
+++ 02_servers/04_routeServer.js
@@ -0,0 +1,21 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("this is home page");
+  } else if (req.url === "/about") {
+    res.end("this is about page");
+  } else {
+    res.end("requested route is not found");
+  }
+});
+
+const port = 5000;
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(err.message);
+  }
+
+  console.log(`server running on port ${port}`);
+});
