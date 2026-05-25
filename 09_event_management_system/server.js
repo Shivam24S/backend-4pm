39f273b310fa1c5157e067435fc56faef81c3492
shivam24s <shivamshrivastav8055@gmail.com>
2026-05-25 17:15:27 +0530

    event management system started

---
+++ 09_event_management_system/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import HttpError from "./middlewares/HttpError.js";
+import connectDB from "./config/db.js";
+
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello from server" });
+});
+
+app.use((req, res, next) => {
+  return next(new HttpError("requested route not found", 404));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({ message: error.message || "internal server error " });
+});
+
+const port = 5000;
+
+async function startServer() {
+  try {
+    const connect = await connectDB();
+
+    if (!connect) {
+      throw new Error("failed to connect db");
+    }
+
+    app.listen(port, (err) => {
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(`server running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
