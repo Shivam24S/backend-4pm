707f77355c04c3293eafaa5aaae3221fd22254af
shivam24s <shivamshrivastav8055@gmail.com>
2026-06-23 17:05:14 +0530

    o-auth initial code setup code added

---
+++ 12_O-auth/model/User.js
@@ -0,0 +1,26 @@
+
+
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    googleId: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+
+
+const User = mongoose.model(UserSchema);
+
+export default User;
---
+++ 12_O-auth/server.js
@@ -0,0 +1,65 @@
+import express from "express";
+
+
+import HttpError from "./middleware/HttpError.js";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js"
+
+import dotenv from "dotenv"
+
+dotenv.config({ path: "./.env" })
+
+const app = express();
+
+
+app.set("view engine", "ejs")
+
+app.use(express.json());
+
+app.use("/auth", authRoutes)
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.use((req, res, next) => {
+    return next(new HttpError("requested route not found", 404));
+});
+
+app.use((error, req, res, next) => {
+    if (res.headersSent) {
+        return next(error);
+    }
+
+    res
+        .status(error.statusCode || 500)
+        .json({ message: error.message || "internal server error" });
+});
+
+const port = process.env.PORT || 5000;
+
+async function startServer() {
+    try {
+        const connect = await connectDB();
+
+        if (!connect) {
+            throw new Error("failed to connect db");
+        }
+        app.listen(port, (err) => {
+            if (err) {
+                return console.log(err.message);
+            }
+
+            console.log(`server running port ${port}`);
+        });
+    } catch (error) {
+        console.log(error.message);
+
+        process.exit(1);
+    }
+}
+
+
+startServer();
