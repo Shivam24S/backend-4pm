dbcb015c219109ac2e719030eb3b35bc4e3f64c3
shivam24s <shivamshrivastav8055@gmail.com>
2026-08-18 14:57:07 +0530

    aggregation code added

--- 13_Meal_Dash/controller/admin.controller.js
+++ 13_Meal_Dash/controller/admin.controller.js
@@ -1,6 +1,9 @@
 import User from "../model/user.model.js";
 
 import HttpError from "../middleware/HttpError.js";
+import restaurantModel from "../model/restaurant.model.js";
+import food from "../model/food.model.js";
+import order from "../model/order.model.js";
 
 const getAllUsers = async (req, res, next) => {
   try {
@@ -28,21 +31,86 @@ const getAllUsers = async (req, res, next) => {
 
     // const totalCount =  await users.countDocument(query);
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "user data fetched successfully",
-        // totalCount,
-        users,
-      });
+    res.status(200).json({
+      success: true,
+      message: "user data fetched successfully",
+      // totalCount,
+      users,
+    });
   } catch (error) {
     return next(new HttpError(error.message, 500));
   }
 };
 
+const dashBoardStatics = async (req, res, next) => {
+  try {
+    const totalUsers = await User.countDocuments();
 
+    const totalCustomer = await User.countDocuments({ role: "customer" });
 
+    const totalApprovedProvider = await User.countDocuments({
+      role: "provider",
+      isVerified: true,
+    });
 
+    const totalPendingProvider = await User.countDocuments({
+      role: "provider",
+      isVerified: false,
+    });
 
-export default {getAllUsers}
+    const totalRestaurant = await restaurantModel.countDocuments();
+
+    const totalApprovedRestaurant = await restaurantModel.countDocuments({
+      isVerified: true,
+    });
+
+    const totalPendingRestaurant = await restaurantModel.countDocuments({
+      isVerified: false,
+    });
+
+    const totalFoodItems = await food.countDocuments();
+
+    const totalApprovedFoodItems = await food.countDocuments({
+      isVerified: true,
+    });
+
+    const totalPendingFoodItems = await food.countDocuments({
+      isVerified: false,
+    });
+
+    const totalBookings = await order.aggregate([
+      {
+        $group: {
+          _id: "$status",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    const totalRevenue = await order.aggregate([
+      {
+        $match: {
+          status: "delivered",
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          revenue: { $sum: "$totalAmount" },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "dashboard data fetched successfully",
+      totalBookings,
+      totalRevenue,
+      totalPendingFoodItems,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+export default { getAllUsers, dashBoardStatics };
--- 13_Meal_Dash/routes/admin.routes.js
+++ 13_Meal_Dash/routes/admin.routes.js
@@ -32,4 +32,9 @@ router.patch(
 
 router.get("/allUsers", auth, checkRole("admin"), adminController.getAllUsers);
 
+
+// dashboard 
+
+router.get("/dashboard",auth,checkRole("admin"),adminController.dashBoardStatics)
+
 export default router;
